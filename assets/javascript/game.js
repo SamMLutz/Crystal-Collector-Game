265e2f7c740fd85e414393dda80f7d0a98320aca
samlutz <sammlutz@gmail.com>
2019-02-26 14:13:20 -0500

    inital html

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// global variables
+var targetNumber = randomNumber;
+
+var wins = 0;
+
+var losses = 0;
+
+var totalScore = 0;
+
+var randomNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
+
+
+$("#target-number").text(randomNumber); 
+
+
+console.log(targetNumber)
